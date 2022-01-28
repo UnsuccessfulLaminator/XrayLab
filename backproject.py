e3685ba3903a10664422341d7c52639f7f10c1ca
TS <threshold_sufferer@localhost.localdomain>
2022-01-28 19:56:56 +0000

    Backprojection script can now operate on multiple files, and has optional parameters to specify output value thresholds. Ray and angle values are hardcoded at the moment, because the PNG meta thing was stupid.

--- backproject.py
+++ backproject.py
@@ -1,28 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
-from PIL.PngImagePlugin import PngImageFile
 import xrayutils
+import os
+from PIL import Image
+import re
+from itertools import count
 
 
 
 parser = ArgumentParser()
-parser.add_argument("projection", help = "Image file containing the xray projection data")
+parser.add_argument("-t0", type = float, help = "Lower threshold for saved image values")
+parser.add_argument("-t1", type = float, help = "Upper threshold for saved image values")
+parser.add_argument("-p", "--plot", action = "store_true")
+parser.add_argument("projection", help = "Image file/dir containing xray projection data")
+parser.add_argument("out_file", help = "Destination/base name for backprojected image(s)")
 
 args = parser.parse_args()
-img = PngImageFile(args.projection)
-ray_xs = np.fromiter(map(float, img.text["ray_xs"].split(",")), dtype = np.float64)
-angles = np.fromiter(map(float, img.text["angles"].split(",")), dtype = np.float64)
+ray_xs = np.linspace(-127.5, 127.5, 256)
+angles = np.linspace(0, np.pi*2, 360, endpoint = False)
+proj_files = []
+out_files = []
 
-proj = np.asarray(img, dtype = np.uint8)
+if os.path.isdir(args.projection):
+    def alpha_num_key(s):
+        return [ int(w) if w.isdigit() else w for w in re.split(r"(\d+)", s) ]
 
-# If the image has multiple color components, average them into one
-if len(proj.shape) == 3: proj = proj[:, :, :3].mean(axis = 2)
+    for filename in sorted(os.listdir(args.projection), key = alpha_num_key):
+        full_name = os.path.join(args.projection, filename)
+        if os.path.isfile(full_name): proj_files.append(full_name)
+    
+    for i in range(len(proj_files)):
+        out_files.append(args.out_file+f"{i}.png")
+elif os.path.isfile(args.projection):
+    proj_files.append(args.projection)
+    out_files.append(args.out_file)
+else:
+    print("Projection file/dir is invalid!")
+    exit(1)
 
-proj = xrayutils.filter_projection(proj, lambda w: np.abs(w))
-backproj = xrayutils.backproject(proj, ray_xs, angles, 256)
+def filter_func(w):
+    return np.abs(w)*np.hanning(w.size*2)[w.size:]
 
-plt.imshow(backproj)
-plt.show()
+for i, in_file, out_file in zip(count(), proj_files, out_files):
+    img = Image.open(in_file)
+    proj = np.asarray(img.convert("I")).astype(np.float64)
+    proj = xrayutils.filter_projection(proj, filter_func)
+    backproj = xrayutils.backproject(proj, ray_xs, angles, 256)
+    
+    if args.plot:
+        plt.imshow(backproj, vmin = args.t0, vmax = args.t1, cmap = "gray")
+        plt.show()
 
-plt.imsave("backproj.png", backproj)
+    plt.imsave(out_file, backproj, vmin = args.t0, vmax = args.t1, cmap = "gray")
+
+    print(f"{i+1}/{len(proj_files)} files completed")
